15997d1d96bf1f3b02105c0cdcd41cd569e0f893
Magi2106 <144230160+Magi2106@users.noreply.github.com>
2023-12-06 18:00:00 +0100

    XD

--- src/main/java/com/example/ice/MenuController.java
+++ src/main/java/com/example/ice/MenuController.java
@@ -1,14 +1,50 @@
 package com.example.ice;
 
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 
-public class MenuController {
-    @FXML
-    private Label welcomeText;
+
+public class MenuController implements Initializable {
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button ExitButton;
+
+    @FXML
+    private Button ShrekButton;
+
+    @FXML
+    private Button Start1;
+
+    @FXML
+    private MediaView mediaView;
+
+    private File file;
+    
+    private Menu menu;
+    private Media media;
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+
+        file = new File("C:\\Users\\Gustav\\Desktop\\Ice\\ICE\\src\\main\\resources\\com\\example\\ice\\DataMonB.mp4");
+        media = new Media(file.toURI().toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+        mediaView.setMediaPlayer(mediaPlayer);
+
     }
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example.ice {
     requires javafx.fxml;
 
     requires com.almasb.fxgl.all;
+    requires javafx.media;
 
     opens com.example.ice to javafx.fxml;
     exports com.example.ice;
