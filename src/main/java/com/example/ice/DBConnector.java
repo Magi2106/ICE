4a138a9823277a26793a490de8743915bef04fa5
Magnewei <143735701+Magnewei@users.noreply.github.com>
2023-12-10 18:54:38 +0100

    Added online database

--- src/main/java/com/example/ice/DBConnector.java
+++ src/main/java/com/example/ice/DBConnector.java
@@ -7,16 +7,16 @@ import java.sql.*;
 import java.util.*;
 public class DBConnector{
     // database URL
-    static final String DB_URL = "jdbc:mysql://localhost/streaming";
+    static final String DB_URL = "jdbc:mysql://sql11.freemysqlhosting.net/user";
 
     //  Database credentials
-    static final String USER = "root";
-    static final String PASS = "1o9B0lu&FCp%";
+    static final String USER = "sql11669270";
+    static final String PASS = "HreeKmVJuh";
 
 
     public List<User> loadUsers() throws FileNotFoundException {
         List<User> users = new ArrayList<>();
-        String selectQuery = "SELECT username, password FROM user";
+        String selectQuery = "SELECT username, password FROM sql11669270.users";
         try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);
              Statement statement = connection.createStatement();
              ResultSet resultSet = statement.executeQuery(selectQuery)) {
@@ -24,10 +24,11 @@ public class DBConnector{
             while (resultSet.next()) {
                 String username = resultSet.getString("username");
                 String password = resultSet.getString("password");
-                int xp = resultSet.getInt("xp");
-                int level = resultSet.getInt("level");
+                 int xp = resultSet.getInt("xp");
+                 int level = resultSet.getInt("level");
+                List<Datamon> list = null;
 
-                users.add(new User(username,password,xp,level));
+                users.add(new User(username,password, list));
 
             }
         } catch (SQLException e) {
@@ -43,7 +44,7 @@ public class DBConnector{
         try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);
              Statement statement = connection.createStatement()){
             String line;
-            String insertQuery = "INSERT INTO streaming.user (username, password,xp,level) VALUES (?, ?, ?, ?)";
+            String insertQuery = "INSERT INTO sql11669270.users (username, password) VALUES (?, ?)";
 
             try (PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
                 // Set parameters based on your CSV file columns and data types
