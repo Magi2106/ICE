d6d0682d0c395d15846a0c6e03ac7a52816b5901
magnewei <magnewei@icloud.com>
2023-12-11 13:28:06 +0100

    Removed test class and FXML file.

--- src/main/java/com/example/ice/User.java
+++ src/main/java/com/example/ice/User.java
@@ -12,7 +12,7 @@ public class User {
     public User(String username, String password) {
         this.username = username;
         this.password = password;
-
+        this.datamons = datamons;
 
     }
     public List<Datamon> getDatamons() {
